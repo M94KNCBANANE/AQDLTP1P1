edc03650d5a6e898e63be413b7d32123121c3820
Rémy Brin-Fortier <remy_b-f@hotmail.com>
2018-03-19 09:19:42 -0400

    Modification Verification

--- TP3-AQDL/src/main/Main.java
+++ TP3-AQDL/src/main/Main.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 
 public class Main {
 	
-	public static String erreur = "Le fichier ne respecte pas le format demandé !";
+	public static String erreur = "";
 	public static NomClient[] tabClient;
 	public static Plats[] tabPlat;
 	public static Commande[] tabCommande;
@@ -112,8 +112,11 @@ public class Main {
 		indiceTexte++;
 		
 		while( !texte[indiceTexte].equals( "Plats :" ) ){
-			
-			tabClient[indiceTab++].setNom( texte[indiceTexte++] );
+			if(Verification.formatClientCorrect(texte[indiceTexte++])){
+			tabClient[indiceTab++].setNom( texte[indiceTexte] );
+			}else{
+				 erreur += "Le nom du client suivant "+ texte[indiceTexte] + " ne respecte pas le format demandé \n";
+			}
 		}
 		
 		indiceTab = 0;
@@ -122,9 +125,14 @@ public class Main {
 		while( !texte[indiceTexte].equals( "Commandes :" ) ){
 			
 			plat = texte[indiceTexte++].split( " " );
-			
+			if(Verification.formatPlatCorrect(plat[0]) && Verification.prixPlatFonctionne(plat[1])){
 			tabPlat[indiceTab].setPlat( plat[0] );			
 			tabPlat[indiceTab++].setPrix( Double.parseDouble( plat[1] ) );
+			}else if(!Verification.formatPlatCorrect(plat[0])){
+				erreur +="Le plat suivant "+plat[0]+" ne respecte pas le format demandé\n";
+			}else{
+				erreur +="Le prix "+plat[1] + " du plat suivant "+plat[0]+" ne respecte pas le format demandé\n";
+			}
 		}
 		
 		indiceTab = 0;
@@ -136,30 +144,25 @@ public class Main {
 			
 			if( plat.length == 3 ){
 				
-				for( int i = 0;i < tabClient.length;i++ ){
-					
-					if( plat[0].equals( tabClient[i].getNom() ) ){
-						
-						for( int j = 0;j < tabPlat.length;j++ ){
-							
-							if( plat[1].equals( tabPlat[j].getPlat() ) ){
-								
-								try{
-									
-									tabCommande[indiceTab].setNom( plat[0] );
-									tabCommande[indiceTab].setPlat( plat[1] );
-									tabCommande[indiceTab++].setQuantite( Integer.parseInt( plat[2] ) );
-									test++;
-									
-								}catch( Exception e ){
-									System.out.println( "Pas une valeur Int possible" );
-								}
-							}
-						}
-					}
+				if(Verification.presenceClient(tabClient,plat[0]) && Verification.presencePlat(tabPlat,plat[1]) && Verification.qtePlatFonctionn(plat[2])){
+					tabCommande[indiceTab].setNom( plat[0] );
+					tabCommande[indiceTab].setPlat( plat[1] );
+					tabCommande[indiceTab++].setQuantite( Integer.parseInt( plat[2] ) );
+					test++;
+				}else if(!Verification.presenceClient(tabClient,plat[0])){
+					erreur += "Le client suivant ";
+				}else if(!Verification.presencePlat(tabPlat,plat[1])){
+					erreur += "Le plat suivant ";
+				}else{
+					erreur += "La quantité";
 				}
+								
+							}
+						
+					
+				
 			}
-		}
+		
 		return test;
 	}
 }
